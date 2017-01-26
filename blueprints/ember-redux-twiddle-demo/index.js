f66055d323828b3f983119322a273fb440c09b90
toran billups <toranb@gmail.com>
2017-01-25 22:42:35 -0600

    v0.0.4 bump

--- blueprints/ember-redux-twiddle-demo/index.js
+++ blueprints/ember-redux-twiddle-demo/index.js
@@ -10,18 +10,13 @@ module.exports = {
         { name: 'ember-fetch', target: '1.3.0' },
         { name: 'ember-cli-mirage', target: '0.2.4' },
         { name: 'ember-lodash-shim', target: '^1.0.0' },
-        { name: 'ember-symbol-observable', target: '0.1.2'},
-        { name: 'ember-redux-shim', target: '^1.0.2' },
-        { name: 'ember-redux-thunk-shim', target: '^1.1.1' }
+        { name: 'ember-symbol-observable', target: '0.1.2'}
       ]
     }).then(function() {
       return this.addPackagesToProject([
         // Installing ember-redux as a package to avoid running its blueprint
         // we want to use the demo blueprint instead
-        { name: 'ember-redux', target: '2.0.0-beta.7' },
-
-        { name: 'redux', target: '3.6.0'},
-        { name: 'redux-thunk', target: '2.2.0'}
+        { name: 'ember-redux', target: '2.0.0-beta.7' }
       ])
     }.bind(this))
   }
