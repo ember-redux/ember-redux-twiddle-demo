ec4b3fab04f801455ded5f809c037e1ddfd98d2f
toran billups <toranb@gmail.com>
2017-08-13 20:22:04 -0500

    bump to v3.0 => migrated to ember-cli-babel ^6.6.0

--- blueprints/ember-redux-twiddle-demo/index.js
+++ blueprints/ember-redux-twiddle-demo/index.js
@@ -7,18 +7,18 @@ module.exports = {
   afterInstall: function() {
     return this.addAddonsToProject({
       packages: [
-        { name: 'ember-fetch', target: '1.3.0' },
-        { name: 'ember-cli-mirage', target: '0.2.4' },
-        { name: 'ember-redux-shim', target: '1.0.2' },
-        { name: 'ember-redux-thunk-shim', target: '1.1.1' }
+        { name: 'ember-fetch', target: '3.2.9' },
+        { name: 'ember-cli-mirage', target: '0.3.4' },
+        { name: 'ember-redux-shim', target: '2.1.0' },
+        { name: 'ember-redux-thunk-shim', target: '2.0.0' }
       ]
     }).then(function() {
       return this.addPackagesToProject([
         // Installing ember-redux as a package to avoid running its blueprint
         // we want to use the demo blueprint instead
-        { name: 'redux', target: '3.6.0' },
+        { name: 'redux', target: '3.7.2' },
         { name: 'redux-thunk', target: '2.2.0' },
-        { name: 'ember-redux', target: '2.0.0' }
+        { name: 'ember-redux', target: '2.10.1' }
       ])
     }.bind(this))
   }
