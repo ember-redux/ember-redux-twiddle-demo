4f06974e7164f90bef941a5ea699f77fd4398f5d
toran billups <toranb@gmail.com>
2017-08-20 15:04:02 -0500

    added ember redux v3.0.0-beta.1

--- blueprints/ember-redux-twiddle-demo/index.js
+++ blueprints/ember-redux-twiddle-demo/index.js
@@ -9,8 +9,8 @@ module.exports = {
       packages: [
         { name: 'ember-fetch', target: '3.2.9' },
         { name: 'ember-cli-mirage', target: '0.3.4' },
-        { name: 'ember-redux-shim', target: '2.1.0' },
-        { name: 'ember-redux-thunk-shim', target: '2.0.0' }
+        { name: 'ember-redux-shim', target: '2.4.0' },
+        { name: 'ember-redux-thunk-shim', target: '2.3.0' }
       ]
     }).then(function() {
       return this.addPackagesToProject([
@@ -18,7 +18,7 @@ module.exports = {
         // we want to use the demo blueprint instead
         { name: 'redux', target: '3.7.2' },
         { name: 'redux-thunk', target: '2.2.0' },
-        { name: 'ember-redux', target: '2.10.1' }
+        { name: 'ember-redux', target: '3.0.0-beta.1' }
       ])
     }.bind(this))
   }
