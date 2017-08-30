1ee8e14b95df6ed39263595cbccf6bb0a4747640
toran billups <toranb@gmail.com>
2017-08-29 20:41:54 -0500

    added normalizr to the demo bundle

--- blueprints/ember-redux-twiddle-demo/index.js
+++ blueprints/ember-redux-twiddle-demo/index.js
@@ -11,6 +11,7 @@ module.exports = {
         { name: 'ember-cli-mirage', target: '0.3.4' },
         { name: 'ember-redux-shim', target: '2.4.0' },
         { name: 'ember-reselect-shim', target: '2.0.0' },
+        { name: 'ember-cli-normalizr-shim', target: '1.0.0' },
         { name: 'ember-redux-thunk-shim', target: '2.3.0' }
       ]
     }).then(function() {
@@ -20,7 +21,8 @@ module.exports = {
         { name: 'redux', target: '3.7.2' },
         { name: 'redux-thunk', target: '2.2.0' },
         { name: 'ember-redux', target: '3.0.0-beta.1' },
-        { name: 'reselect', target: '3.0.1' }
+        { name: 'reselect', target: '3.0.1' },
+        { name: 'normalizr', target: '3.2.2' }
       ])
     }.bind(this))
   }
